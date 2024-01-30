8e3cc8232f7186d0ef11a9c2f11a3c08da5664a8
Pablo Bracciale <pbracciale86@gmail.com>
2024-01-29 21:02:16 -0300

    agregando estilo a primeros componentes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
 function Header () {
     return (
-        <h1> Desde header </h1>
+        <h1 className="font-black text-5xl text-center w-1/2 mx-auto"> Seguimiento veterinaria {" "}
+            <span className="text-indigo-600">Veterinaria</span>
+        </h1>
+            
     )
 }
 
