667b8b6aaa9db5698d711cdd68ab1a08a348b182
Pablo Bracciale <pbracciale86@gmail.com>
2024-02-04 10:35:07 -0300

    Dandole estilo al input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,12 +2,14 @@ let Formulario = () => {
     return (
         <div className="md:w-1/2 lg:w-2/5">
             <h2 className="font-black text-3xl text-center"> Seguimiento Pacientes </h2>
-            <p className="text-lg mt-5 text-center">Añade pacientes y {" "}</p>
+            <p className="text-lg mt-5 text-center mb-10">Añade pacientes y {" "}
             <span className="text-indigo-600 font-bold">Administralos</span>
+            </p>
             <form className="bg-white shadow-md rounded-lg py-10 px-5">
                 <div>
-                    <label className="block"> Nombre mascota </label>
-                    <input type="text" placeholder="Nombre de la mascota"/>
+                    <label className="block text-gray-700 uppercase font-bold" htmlFor="mascota"> Nombre mascota </label>
+                    <input id="mascota" type="text" placeholder="Nombre de la mascota" className="border-2 w-full  p-2 mt-2 
+                    placeholder-gray-400 rounded-md"/>
                 </div>
             </form>
         </div>
