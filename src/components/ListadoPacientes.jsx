46203b53804d73fd08d0770fd4a9e9cef36ab506
Pablo Bracciale <pbracciale86@gmail.com>
2024-02-06 07:59:37 -0300

    Creando componente paciente

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,13 @@
+import Paciente from "./Paciente";
+
 let ListadoPacientes = () => {
     return (
-        <div className="md:w-1/2 lg:w-3/5">
-            <h2>Listado pacientes</h2>
+        <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+            <h2 className="font-black text-3xl text-center">Listado pacientes</h2>
+            <p className="text-xl mt-5 mb-10 text-center">Administra tus {" "}
+                <span className="text-indigo-600 font-bold"> Pacientes y citas </span>
+            </p>
+        <Paciente/>
         </div>)
 }
 
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,23 @@
+let Paciente = () => {
+    return (         
+        <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+            <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {" "}
+                <span className="font-normal normal-case"> Hook </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {" "}
+                <span className="font-normal normal-case"> Matias </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Email: {" "}
+                <span className="font-normal normal-case"> correo@correo.com </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Fecha alta: {" "}
+                <span className="font-normal normal-case"> 10/10/2022 </span>
+            </p>
+            <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {" "}
+                <span className="font-normal normal-case"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque, rerum odio amet dolores libero ex repellendus aspernatur quisquam nihil tenetur est minus sit maxime eos ab, optio alias. Architecto, fuga. </span>
+            </p>
+        </div>
+    )
+}
+
+export default Paciente;
